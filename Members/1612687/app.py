bd9f923592aaac15ffffb441010e680122ad7cca
nguyentien121215 <nguyentien.121215@gmail.com>
2020-03-15 23:45:22 +0700

    Kết nối CSDL MYSQL

--- Members/1612687/app.py
+++ Members/1612687/app.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template, redirect, url_for
-import sqlite3
+import mysql.connector
 app = Flask(__name__)
+mydb = mysql.connector.connect(
+  host="sql12.freemysqlhosting.net",
+  user="sql12328142",
+  passwd="iEhPW1Zn9E",
+  database="sql12328142"
+)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM category")
+    myresult = mycursor.fetchall()
+    print(type(myresult), flush=True)
+    for x in myresult:
+        print(x)
+    return render_template('index.html', list = myresult)
 
 @app.route('/<category>')
 def category(category):
-    return render_template('shoe.html')
-
+    return render_template('category.html')
 
 if __name__ == '__main__':
    app.run()
