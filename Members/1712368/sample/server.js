a985c9cb812f3821deb6d5dceb00ce0809c6ae3c
meokg456 <meokg456@gmail.com>
2020-03-17 18:38:49 +0700

    Initial heroku

--- Members/1712368/sample/server.js
+++ Members/1712368/sample/server.js
@@ -3,42 +3,42 @@ const app = require("./backend/app");
 const debug = require("debug");
 
 const normalizePort = val => {
-    var port = parseInt(val, 10);
-    if (isNaN(port)) {
-        //named pipe
-        return val;
-    }
-    if (port >= 0) {
-        //port number
-        return port;
-    }
-    return false;
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    //named pipe
+    return val;
+  }
+  if (port >= 0) {
+    //port number
+    return port;
+  }
+  return false;
 };
 
 const onError = error => {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
-    const bind = typeof addr === "string" ? "pipe" + addr : "port" + port;
-    switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requires elevated privileges");
-            process.exit(1);
-            break;
-        case "EADDRINUSE":
-            console.error(bind + " is aldready in use");
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof addr === "string" ? "pipe" + addr : "port" + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is aldready in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 };
 const onListening = () => {
-    const addr = server.address();
-    const bind = typeof addr === "string" ? "pipe" + addr : "port" + port;
-    debug("Listening on" + bind);
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe" + addr : "port" + port;
+  debug("Listening on" + bind);
 };
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT || "8080");
 
 app.set("port", port);
 const server = http.createServer(app);
