3778a0b138cebc4013a237e24c279556020e456b
khaitranhuynh <38784446+khaitranhuynh@users.noreply.github.com>
2020-03-18 18:32:00 +0700

    router + controller

---
+++ Members/1712828/routes/indexRouter.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    let categoryController = require('../controllers/categoryController');
+    categoryController
+        .getAll()
+        .then(data => {
+            res.locals.categories = data;
+            res.render('index');
+        })
+        .catch(error => next (error));
+ 
+});
+
+module.exports = router;
