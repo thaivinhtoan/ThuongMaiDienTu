90b368f4daa08ba02e40b6733dd6a3cf26c9cdad
Phan Thị Mỹ Diễm - 1712341 <phandiem1499@gmail.com>
2020-08-07 20:30:54 +0700

    [UPDATE] Building Contact Basic + edit interface

--- Projects/index.js
+++ Projects/index.js
@@ -58,6 +58,41 @@ app.use((req, res, next) => {
     next();
 });
 
+// Use nodemailer
+const nodemailer = require('nodemailer');
+// POST route from contact form
+app.post('/contact', (req, res) => {
+    const GMAIL_USER = process.env.username
+    const GMAIL_PASS = process.env.password
+        // Instantiate the SMTP server
+    const smtpTrans = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: GMAIL_USER,
+            pass: GMAIL_PASS
+        }
+    })
+
+    // Specify what the email will look like
+    const mailOpts = {
+        from: 'Your sender info here', // This is ignored by Gmail
+        to: GMAIL_USER,
+        subject: 'New message from contact form at tylerkrys.ca',
+        text: `${req.body.name} (${req.body.username}) says: ${req.body.message}`
+    }
+
+    // Attempt to send the email
+    smtpTrans.sendMail(mailOpts, (error, response) => {
+        if (error) {
+            res.render('contact-failure') // Show a page indicating failure
+        } else {
+            res.render('contact-success') // Show a page indicating success
+        }
+    })
+})
+
 app.use('/', require('./routes/indexRouter'));
 app.use('/courses', require('./routes/productRouter'));
 app.use('/users', require('./routes/userRouter'));
