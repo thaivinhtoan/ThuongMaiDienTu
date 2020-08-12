93ee60b31bd2a03b5eef97dbc4fa02ffc11b84c0
Thai Vinh Toan <vinhtoan466@gmail.com>
2020-08-12 21:41:25 +0700

    Bao mat thanh toan paypal bang api

--- Projects/routes/cartRouter.js
+++ Projects/routes/cartRouter.js
@@ -1,45 +1,131 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
+let paypal = require("paypal-rest-sdk");
 
-router.get('/', (req, res, next) => {
-    var cart = req.session.cart;
-    res.locals.cart = cart.getCart();
-    res.render('cart');
+router.get("/", (req, res, next) => {
+  var cart = req.session.cart;
+  res.locals.cart = cart.getCart();
+  res.render("cart");
 });
 
-router.post('/', (req, res, next) => {
-    var courseId = req.body.id;
-    var quantity = isNaN(req.body.quantity) ? 1 : req.body.quantity;
-    var courseController = require('../controllers/productController');
-    courseController
-        .getById(courseId)
-        .then(course => {
-            var cartItem = req.session.cart.add(course, courseId, quantity);
-            res.json(cartItem);
-        })
-        .catch(error => next(error));
+router.post("/", (req, res, next) => {
+  var courseId = req.body.id;
+  var quantity = isNaN(req.body.quantity) ? 1 : req.body.quantity;
+  var courseController = require("../controllers/productController");
+  courseController
+    .getById(courseId)
+    .then((course) => {
+      var cartItem = req.session.cart.add(course, courseId, quantity);
+      res.json(cartItem);
+    })
+    .catch((error) => next(error));
 });
 
-router.put('/', (req, res) => {
-    var courseId = req.body.id;
-    var quantity = parseInt(req.body.quantity);
-    var cartItem = req.session.cart.update(courseId, quantity);
-    res.json(cartItem);
+router.put("/", (req, res) => {
+  var courseId = req.body.id;
+  var quantity = parseInt(req.body.quantity);
+  var cartItem = req.session.cart.update(courseId, quantity);
+  res.json(cartItem);
 });
 
-router.delete('/', (req, res) => {
-    var courseId = req.body.id;
-    req.session.cart.remove(courseId);
-    res.json({
-        totalQuantity: req.session.cart.totalQuantity,
-        totalPrice: req.session.cart.totalPrice
-    });
+router.delete("/", (req, res) => {
+  var courseId = req.body.id;
+  req.session.cart.remove(courseId);
+  res.json({
+    totalQuantity: req.session.cart.totalQuantity,
+    totalPrice: req.session.cart.totalPrice,
+  });
 });
 
-router.delete('/all', (req, res) => {
-    req.session.cart.empty();
-    res.sendStatus(204);
-    res.end();
+router.delete("/all", (req, res) => {
+  req.session.cart.empty();
+  res.sendStatus(204);
+  res.end();
 });
+
+paypal.configure({
+  mode: "sandbox", //sandbox or live
+  client_id:
+    "ASFthYP4yc_HJ4s3FHW9XqclQ7R2TeTnEFjcFoziey5eoPDOeFwszehw8ojqBpH3_UvNx0G5F_XVIQVE",
+  client_secret:
+    "ECDD_KQj8uXbZeoizVr47NfwM0Fzw-tSOQfO42LIUEP_x04G-ba-xdNgYem0bb0qeQPDTPtB6wj6W8Gh",
+});
+
+//let app = express();
+router.post("/pay", (req, res) => {
+  var create_payment_json = {
+    intent: "sale",
+    payer: {
+      payment_method: "paypal",
+    },
+    redirect_urls: {
+      return_url: "http://localhost:5000/",
+      cancel_url: "http://localhost:5000/cart",
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: "MUA KHOA HOC",
+              sku: "001",
+              price: req.session.cart.totalPrice,
+              currency: "USD",
+              quantity: 1,
+            },
+          ],
+        },
+        amount: {
+          currency: "USD",
+          total: req.session.cart.totalPrice,
+        },
+        description: "This is the payment description.",
+      },
+    ],
+  };
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+      throw error;
+    } else {
+      for (let i = 0; i < payment.links.length; i++) {
+        if (payment.links[i].rel === "approval_url") {
+          res.redirect(payment.links[i].href);
+        }
+      }
+    }
+  });
+});
+
+router.get("/success", (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+
+  const execute_payment_json = {
+    payer_id: payerId,
+    transactions: [
+      {
+        amount: {
+          currency: "USD",
+          total: req.session.cart.totalPrice,
+        },
+      },
+    ],
+  };
+
+  paypal.payment.execute(paymentId, execute_payment_json, function (
+    error,
+    payment
+  ) {
+    if (error) {
+      console.log(error.response);
+      throw error;
+    } else {
+      console.log(JSON.stringify(payment));
+      res.render("/");
+    }
+  });
+});
+
+router.get("/cancel", (req, res) => res.render("cart"));
 
 module.exports = router;
