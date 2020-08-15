5ef2d87ca37017fabcce99010993da1b16fd34ac
nguyentien121215 <nguyentien.121215@gmail.com>
2020-08-15 16:52:22 +0700

    cập nhật thêm chức năng của admin và chỉnh lý lại class

--- Projects/controllers/adminController.js
+++ Projects/controllers/adminController.js
@@ -14,5 +14,20 @@ controller.getAmountBillCategory = async () =>{
 }
 
 
+controller.getAmountUser = async () =>{
+    
+    const hd= await models.sequelize.query(`Select count(*) as sl from users`, { type: models.Sequelize.QueryTypes.SELECT})
+    return hd
+}
 
+controller.getAmountBillteacher = async () =>{
+    
+    const hd= await models.sequelize.query(`Select count(b.*) as sl, c.name , c.imagepath
+    from teachers c full outer join 
+    (select courses.* from courses, invoicedetails where courses.id = invoicedetails.courseid ) b 
+    on c.id= b.teacherid 
+    group by c.name, c.imagepath
+    `, { type: models.Sequelize.QueryTypes.SELECT})
+    return hd
+}
 module.exports = controller;
--- Projects/routes/adminRouter.js
+++ Projects/routes/adminRouter.js
@@ -6,6 +6,13 @@ router.get('/', userController.isLoggedIn,async(req, res, next) => {
     userController.isLoggedIn
     let hoaDon_c = await adminController.getAmountBillCategory();
     res.locals.hoaDon_c = hoaDon_c;
+
+    let sluser = await adminController.getAmountUser();
+    res.locals.sluser = sluser;
+
+    let hoaDon_t = await adminController.getAmountBillteacher();
+    res.locals.hoaDon_t = hoaDon_t;
+    
     res.render('admin')
 });
 module.exports = router;
